156f22219d1dab0f61af997730b077fc73703450
Tamas Nepusz <ntamas@gmail.com>
2017-10-19 12:15:16 +0200

    removed react-hot-loader form webpack.config.js because it was outdated

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,10 @@ var webpack = require('webpack');
 module.exports = {
   entry: [
     "webpack-dev-server/client?http://0.0.0.0:8080",
-    'webpack/hot/only-dev-server',
     './examples/index'
   ],
   devServer: {
-    contentBase: './examples/',
-    hot: true
+    contentBase: './examples/'
   },
   devtool: "eval",
   debug: true,
@@ -21,7 +19,6 @@ module.exports = {
     modulesDirectories: ['node_modules']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.IgnorePlugin(/un~$/)
   ],
@@ -31,7 +28,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loaders: ['style', 'css']},
-      { test: /\.cjsx$/, loaders: ['react-hot', 'coffee', 'cjsx']},
+      { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
       { test: /\.coffee$/, loader: 'coffee' }
     ]
   }
